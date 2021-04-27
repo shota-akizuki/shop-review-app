3328a47772ab8c0bcb6bfd24ae2678c69fe7ee65
Shota <64979430+shota-akizuki@users.noreply.github.com>
2021-04-28 02:08:28 +0900

    React Nativeでalgoliaの全文検索を実行する

--- src/navigation/MainTabNavigator.tsx
+++ src/navigation/MainTabNavigator.tsx
@@ -5,6 +5,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { HomeStackNavigator } from "./HomeStackNavigator";
 import { UserScreen } from "../lib/screens/UserScreen";
 import { Feather } from "@expo/vector-icons";
+import { SearchScreen } from "../lib/screens/SearchScreen";
 
 const Tab = createBottomTabNavigator();
 
@@ -22,6 +23,16 @@ export const MainTabNavigator = () => {
           tabBarLabel: "Home",
           tabBarIcon: ({ color, size }) => (
             <Feather name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={SearchScreen}
+        options={{
+          tabBarLabel: "Search",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="search" color={color} size={size} />
           ),
         }}
       />
